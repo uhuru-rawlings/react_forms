aa4846f8f94a44bb1097e7b48ff4354c84647a14
uhururawlings40 <uhururawlings40@gmail.com>
2022-09-07 21:09:37 +0300

    added login form

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import './Login.css'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='user_authentication'>
+        <form action="" method="post" autoComplete='off'>
+            <h2 className='text-center'>Login Form</h2>
+            <p className='text-center'>Please fill the for and let's get you loged in</p>
+            <div className="form-group">
+                <input type="email" name="useremail" id="useremail" className="form-control" placeholder="Email" />
+            </div>
+            <div className="form-group">
+                <input type="password" name="passwords" id="passwords" className="form-control" placeholder="Password" />
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Login" className="btn btn-primary w-100" />
+            </div>
+            <div className="form-group">
+                <div className="row">
+                    <div className="col">
+                        <Link to={"./signup"}>Don't have account ?</Link>
+                    </div>
+                    <div className="col">
+                        <Link to={"./reset_password"}>Forgot password ?</Link>
+                    </div>
+                </div>
+            </div>
+            <p className="terms text-center">copyright &copy; 2022 Terms and conditions aply</p>
+        </form>
+    </div>
   )
 }
