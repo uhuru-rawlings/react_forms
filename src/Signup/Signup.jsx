a554c812a1a666e997eb3e61e671013d3259eb08
uhururawlings40 <uhururawlings40@gmail.com>
2022-09-07 22:44:27 +0300

    added signup validation

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React,{ useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Signup = () => {
+    const username = useRef();
+    const useremail = useRef();
+    const passwords = useRef();
+    const passwordagain = useRef();
+
+    const validateSignup = (e) => {
+        e.preventDefault();
+        if(username.current.value === "" || useremail.current.value === "" || passwords.current.value === ""){
+            if(username.current.value === ""){
+                alert("please provide username to continue");
+            }else if(useremail.current.value === ""){
+                alert("please provide email adresss to continue");
+            }else{
+                alert("please provide password to continue");
+            }
+        }else{
+            if(passwords.current.value !== passwordagain.current.value){
+                alert("password do not match please confirm password.");
+            }
+        }
+    }
   return (
     <div className='user_authentication'>
-        <form action="" method="post" autoComplete='off'>
+        <form action="" onSubmit={ validateSignup } method="post" autoComplete='off'>
             <h2 className='text-center'>Reset Password</h2>
             <p className='text-center'>Please fill the form to reset password.</p>
             <div className="form-group">
-                <input type="text" name="username" id="username" className="form-control" placeholder="Username" />
+                <input type="text" ref={ username } name="username" id="username" className="form-control" placeholder="Username" />
             </div>
             <div className="form-group">
-                <input type="email" name="useremail" id="useremail" className="form-control" placeholder="Email" />
+                <input type="email" ref={ useremail } name="useremail" id="useremail" className="form-control" placeholder="Email" />
             </div>
             <div className="form-group">
-                <input type="password" name="passwords" id="passwords" className="form-control" placeholder="Password" />
+                <input type="password" ref={ passwords } name="passwords" id="passwords" className="form-control" placeholder="Password" />
             </div>
             <div className="form-group">
-                <input type="password" name="passwordagain" id="passwordagain" className="form-control" placeholder="Confirm Password" />
+                <input type="password" ref={ passwordagain } name="passwordagain" id="passwordagain" className="form-control" placeholder="Confirm Password" />
             </div>
             <div className="form-group">
                 <input type="submit" value="Signup" className="btn btn-primary w-100 shadow-none" />
